b9a32d22e81ad5e5627546a37929ae91203bb2e4
Rayyan Gulshan Khan <khan274@xinu04.cs.purdue.edu>
2023-04-04 23:43:03 -0400

    starting part 4

---
+++ lab4/xinu-spring2023/system/cbchildregister.c
@@ -0,0 +1,11 @@
+/* cbchildregister.c - cbchildregister */
+
+#include <xinu.h>
+
+/*------------------------------------------------------------------------
+ *  cbchildregister - register a callback function for a new processz
+ *------------------------------------------------------------------------
+ */
+extern syscall cbchildregister(void (*cbf), (void)) {
+    return 0;
+}
